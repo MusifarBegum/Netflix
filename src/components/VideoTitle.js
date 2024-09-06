fe91e1ec979a5d0ec537722a89768bde2d616e24
musifar.begum <musifar.begum@gmail.com>
2024-09-06 17:08:58 +0530

    VideoTitle Responsive for md and lg

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const VideoTitle = ({title,overview}) => {
   return (
-    <div className='w-screen aspect-video pt-[18%] px-12 absolute text-white bg-gradient-to-r from-black'>
-      <h1 className='sm:text-xl sm:w-1/4 md:text-3xl font-bold py-2'>{title}</h1>
-      <p className=' sm:hidden md:block md:py-6 text-sm w-1/4'>{overview}</p>
+    <div className='w-screen aspect-video md:pt-[20%] lg:pt-[18%] px-12 absolute text-white bg-gradient-to-r from-black'>
+      <h1 className='w-1/4 md:text-3xl font-bold py-2'>{title}</h1>
+      <p className='md: hidden lg:block md:py-6 text-sm w-1/4'>{overview}</p>
       <div className='pt-2'>
         <button className='px-12  p-4 bg-white text-black rounded-lg text-xl hover:bg-opacity-50'>  ▷  Play</button>
         <button className='mx-2 px-12  p-4 bg-gray-500 text-white rounded-lg text-xl bg-opacity-50 hover:bg-opacity-30'>ⓘ More info</button>
